c00a635eefbea3cab5b25dc0f2636e455ce27462
ShahabBngsh <shahabbng1999@gmail.com>
2020-11-28 13:18:12 +0500

    Server can now read and write through select() Detail: select() will now look for all read file descriptors as well as write file discriptors

--- server.cpp
+++ server.cpp
@@ -1,7 +1,7 @@
 #include "libraries.h"
 using namespace std;
 
-#define MAXFD 3	//Size of fds array
+#define MAXFD 4	//Size of fds array
 #define PORTNO 6000
 #define backlog 3
 #define localhost "127.0.0.1"
@@ -10,11 +10,12 @@ using namespace std;
 int id = 0;
 int tickarr[backlog]={0};
 
-void fds_init(int fds[],int fd) {	//Add a file descriptor to the fds array
+//Add a file descriptor to the fds array
+void fds_init(int fds_arr[],int fd) {
 	int i=0;
 	for( ; i<MAXFD; ++i) {
-		if(fds[i]==-1) {
-			fds[i]=fd;
+		if(fds_arr[i]==-1) {
+			fds_arr[i]=fd;
 			break;
 		}
 	}
@@ -25,6 +26,12 @@ void checkError(const int& ret, const int& flag,  const string& text) {
 		cout << "ERROR: " << text << " flag: " << flag;
 		exit(-1);
 	}
+}
+int acceptClient(int sockfd) {
+	struct sockaddr_in caddr;
+	socklen_t len=sizeof(caddr);
+	//Accept new client connections
+	return accept(sockfd,(struct sockaddr *)&caddr, &len);
 }
 
 int main() {
@@ -44,89 +51,117 @@ int main() {
 	//Create listening queue
 	listen(sockfd, backlog);
 	//Define fdset collection
-	fd_set fdset;
+	fd_set readfds, writefds;
 	
 	//Define fds array
-	int fds[MAXFD];
-	int i=0;
-	for(;i<MAXFD;++i) {
-		fds[i]=-1;
+	int readfds_arr[MAXFD], writefds_arr[MAXFD];
+	for(int i=0; i<MAXFD; ++i) {
+		readfds_arr[i]  = -1;
+		writefds_arr[i] = -1;
 	}
 	
 	//Add a file descriptor to the fds array
-  fds_init(fds,sockfd);
+  fds_init(readfds_arr,  sockfd);
+	fds_init(writefds_arr, sockfd);
 
 	while(1) {
-		FD_ZERO(&fdset);//Clear the fdset array to 0
+		FD_ZERO(&readfds);//Clear the readfds array to 0
+		FD_ZERO(&writefds);//Clear the writefds array to 0
 		int maxfd=-1;
 
-		int i=0;
 		//For loop finds the maximum subscript for the ready event in the fds array
-		for( ;i<MAXFD;i++)	{
-			if(fds[i]==-1) {
+		for(int i=0; i<MAXFD; i++)	{
+			if(readfds_arr[i]==-1) {
 				continue;
 			}
-
-			FD_SET(fds[i],&fdset);
-
-			if(fds[i]>maxfd) {
-				maxfd=fds[i];
+			FD_SET(readfds_arr[i],  &readfds);
+			if(readfds_arr[i] > maxfd) {
+				maxfd = readfds_arr[i];
+			}
+		}
+		for(int i=0; i<MAXFD; i++)	{
+			if(writefds_arr[i]==-1) {
+				continue;
+			}
+			FD_SET(writefds_arr[i],  &writefds);
+			if(writefds_arr[i] > maxfd) {
+				maxfd = writefds_arr[i];
 			}
 		}
 
-		struct timeval tv={5,0};	//Set timeout of 5 seconds
-
-		int retval=select(maxfd+1,&fdset,NULL,NULL,&tv);//Selectect system call, where we only focus on read events
-		if(retval==-1)	{ //fail
+		struct timeval tv = {15, 0};
+		//Selectect system call, where we only focus on read events
+		int retval = select(maxfd+1, &readfds, &writefds, NULL,&tv);
+		if(retval == -1)	{ //fail
 			perror("ERROR: select()");
 		}
-		else if(retval==0) { //Timeout, meaning no file descriptor returned
+		else if(retval == 0) { //Timeout, meaning no file descriptor returned
 			printf("time out\n");
 		}
 		else { //Ready event generation
-		//Because we only know the number of ready events by the return value of select, we don't know which events are ready.
-		//Therefore, each file descriptor needs to be traversed for judgment
-			for(i=0;i<MAXFD;++i)	{
-				if(fds[i]==-1) {	//If fds[i]==-1, the event is not ready
+			//loop through each readfd, to see if it has something to read from
+			for(int i=0; i < MAXFD; ++i)	{
+				if(readfds_arr[i]==-1) {	//If readfds_arr[i]==-1, the event is not ready
 					continue;
-				}
-				if(FD_ISSET(fds[i],&fdset)) {	//Determine if the event corresponding to the file descriptor is ready
-				//There are two kinds of cases for judging file descriptors
-					if(fds[i]==sockfd) {	//A file descriptor is a socket, meaning accept if a new client requests a connection
-						struct sockaddr_in caddr;
-						socklen_t len=sizeof(caddr);
+				}//Determine if the event corresponding to the file descriptor is ready
+				if(FD_ISSET(readfds_arr[i], &readfds)) {
+					//accept if a new client requests a connection
+					if(readfds_arr[i]==sockfd) {
+
 						//Accept new client connections
-						int retval = accept(sockfd,(struct sockaddr *)&caddr, &len);
+						int retval = acceptClient(sockfd);
 						if(retval < 0) {
+							continue;
+						} else if(retval > MAXFD+2) {
+							printf("WARNING: Server limit exceeded\nrequest declined!\n");
 							continue;
 						}
 						printf("accept c=%d\n",retval);
-						fds_init(fds, retval);//Add the connection socket to the array where the file descriptor is stored
+						fds_init(readfds_arr, retval);//Add the connection socket to the readfds_arr array
 					} else {  //Receive data recv when an existing client sends data
-						char buff[128]={0};
-						int res=recv(fds[i],buff,BUFFSIZE,0);
-						checkError(res,errno, "recv\n");
-						if(res == 0) {
-							close(fds[i]);
-							fds[i]=-1;
+						char buff[BUFFSIZE]={0};
+						int res=recv(readfds_arr[i],buff,BUFFSIZE,0);
+						if(res <= 0) {
+							close(readfds_arr[i]);
+							readfds_arr[i]=-1;
 							printf("client disconnected!\n");
-						}
-						else
-						{
-							printf("recv(%d)=%s\n",fds[i],buff);	//Output Client Sent Information
-							//send(fds[i],"",0,0);
+						} else {
+							printf("recv(%d)=%s\n", readfds_arr[i], buff);	//Output Client Sent Information
+							//send(readfds_arr[i],"",0,0);
 							
-							printf("id: %d\n", id);
-							printf("Updated ticket array:\n");
-							for (int i = 0; i < backlog; i++) {
-								printf("%d ", tickarr[i]);
-							}
-							printf("\n");
-								
 						}
 					}
 				}
 			}
+			//loop through each writefd, to see if it has something to write to
+			for(int i=0; i < MAXFD; ++i)	{
+				if(writefds_arr[i]==-1) {	//If writefds_arr[i]==-1, the event is not ready
+					continue;
+				}//Determine if the event corresponding to the file descriptor is ready
+				if(FD_ISSET(writefds_arr[i], &writefds)) {
+					//accept if a new client requests a connection
+					if(writefds_arr[i]==sockfd) {
+						//Accept new client connections
+						int retval = acceptClient(sockfd);
+						if(retval < 0) {
+							continue;
+						}
+						printf("accept c=%d\n",retval);
+						fds_init(writefds_arr, retval);//Add the connection socket to the readfds_arr array
+					} else {  //Receive data recv when an existing client sends data
+						char buff[BUFFSIZE] = "---lol---\n";
+						int retval = send(writefds_arr[i], buff, sizeof(buff), 0);
+						if(retval <= 0) {
+							close(writefds_arr[i]);
+							writefds_arr[i]=-1;
+							printf("client disconnected!\n");
+						} else {
+							printf("sent bytes: %d  -> client: %d\n", retval, i);
+						}
+					}
+				}
+			}
+		
 		}
 	}
 }
