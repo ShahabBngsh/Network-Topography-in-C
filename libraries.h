bc6a00ffa2bf73c1ded3436ab1e946a2ec651a7a
ShahabBngsh <shahabbng1999@gmail.com>
2020-11-28 22:26:01 +0500

    moved all common function to libraries.h

--- libraries.h
+++ libraries.h
@@ -11,3 +11,33 @@
 #include <string>
 #include <iostream>
 #include <functional>
+
+#define localhost "127.0.0.1"
+#define MSPORTNO 7000
+#define backlog 3
+#define MAXFD 4	//Size of fds array
+#define BUFFSIZE 127
+
+
+//Add a file descriptor to the fds array
+void fds_init(int fds_arr[],int fd) {
+	for(int i=0; i<MAXFD; ++i) {
+		if(fds_arr[i]==-1) {
+			fds_arr[i]=fd;
+			break;
+		}
+	}
+}
+
+void checkError(const int& ret, const char* s) {
+	if(ret == -1) {
+		perror(s);
+		exit(-1);
+	}
+}
+int acceptClient(int sockfd) {
+	struct sockaddr_in caddr;
+	socklen_t len=sizeof(caddr);
+	//Accept new client connections
+	return accept(sockfd,(struct sockaddr *)&caddr, &len);
+}
